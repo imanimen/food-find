33658724afa65bb9775cde214ba485ade71b83c2
iman <imanimen81@gmail.com>
2024-03-27 09:26:43 +0330

    [ADD] avatar attr

--- models/user.go
+++ models/user.go
@@ -1,11 +1,18 @@
 package models
 
+import "os"
+
 type User struct {
-	ID  		string  `json:"id"`
-	Username 	string  `json:"username"`
-	Email 		string  `json:"email"`
-	Lat 	  	string  `json:"lat"`
-	Long  	  	string  `json:"long"`
-	CreatedAt   string  `json:"created_at"`
-	UpdatedAt   string 	`json:"updated_at"`
+	ID        string `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Lat       string `json:"lat"`
+	Long      string `json:"long"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	Avatar    string `json:"avatar"`
+}
+
+func (u *User) GetAvatar() string {
+	return os.Getenv("CDN_URL")+u.Avatar
 }
